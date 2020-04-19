5e7b6c714b71904451acba0cf7699958946f1d07
Anusha Yalakala <anushayalakala@gmail.com>
2020-04-18 22:32:11 -0500

    git commands testing

---
+++ fileread.js
@@ -0,0 +1,8 @@
+
+const fs = require('fs') 
+  
+fs.readFile('/Users/anushayalakala/Desktop/test.txt', (err, data) => { 
+    if (err) throw err; 
+    let processedData = data.toString().replace(/\d/g,'')
+    console.log(processedData); 
+}) 
